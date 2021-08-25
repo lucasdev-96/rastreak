7c94ee4efcc92bfff3dc942fc4ab8871caf18c54
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-08-25 12:46:18 -0300

    estilizando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import '../styles/Footer.css';
+import logoFooter from '../img/logo-footer.png';
+import googleplay from '../img/playstore-button.png';
+import apple from '../img/apple.png';
 
 function Footer() {
   return (
-    <div className="footer_father">
-      oi
-    </div>
+    <footer className="footer_father">
+      <section>
+        <div className="footer_info">
+          <img src={logoFooter} className="logo" alt="logo" />
+          <p>Baixe nosso Aplicativo</p>
+          <div className="apps">
+            <button type="button">
+              <img src={googleplay} alt="googlePlay" style={{ width: '150px' }} />
+            </button>
+            <button type="button">
+              <img src={apple} alt="googlePlay" style={{ width: '150px' }} />
+            </button>
+          </div>
+        </div>
+        <div>
+          oi
+        </div>
+      </section>
+    </footer>
   );
 }
 
