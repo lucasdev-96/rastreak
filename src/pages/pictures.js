3934dc95e2c75f55aa68ac79ac56f2fa8b79bd0a
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-09-05 15:53:18 -0300

    Arruma layout

--- src/pages/pictures.js
+++ src/pages/pictures.js
@@ -5,14 +5,16 @@ import arrayPictures from '../services/arrayOfPictures';
 
 function pictures() {
   return (
-    <div className="pattern_pictures">
+    <>
       <Header />
-      <section className="father_pictures">
-        {arrayPictures.map((elem) => (
-          <img className="pictures" key={elem} src={elem} alt="rastreak" />
-        ))}
-      </section>
-    </div>
+      <div className="pattern_pictures">
+        <section className="father_pictures">
+          {arrayPictures.map((elem) => (
+            <img className="pictures" key={elem} src={elem} alt="rastreak" />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
