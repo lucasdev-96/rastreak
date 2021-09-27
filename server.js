3748310cf4b9100584eff04e1e14055780e37c18
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-09-27 18:41:22 -0300

    config server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('aqui')
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '.', 'build')));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) console.log(err);
+  console.log('tudo ok');
+});
